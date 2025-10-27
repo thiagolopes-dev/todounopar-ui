139f2e0c4b96d0c6f409e424d2a55a932a0294fd
Thiago Lopes <thiagoolopezz@gmail.com>
2025-10-27 19:31:01 -0300

    add primeng, primeflex e primeicons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TaskListComponent } from './tasks/task-list/task-list.component';
-import { TaskListItemComponent } from './tasks/task-list-item/task-list-item.component';
+import { PrimeNGModule } from './primeng.module';
 import { TaskFormComponent } from './tasks/task-form/task-form.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { TaskListItemComponent } from './tasks/task-list-item/task-list-item.component';
+import { TaskListComponent } from './tasks/task-list/task-list.component';
 
 
 @NgModule({
@@ -23,7 +24,8 @@ import { FormsModule } from '@angular/forms';
     BrowserAnimationsModule,
     HttpClientModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PrimeNGModule
   ],
   providers: [],
   bootstrap: [AppComponent]
